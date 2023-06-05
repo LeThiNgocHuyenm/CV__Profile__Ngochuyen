c7117545ac0d5f9d9ae2c5c4094b5d157a10aa1d
LeThiNgocHuyenm <ngochuyenn0502@gmail.com>
2023-06-05 20:16:04 +0700

    CV-Profile

--- src/config/connect_db.ts
+++ src/config/connect_db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connect = () => {
   mongoose
-    .connect('mongodb://localhost:27017/thuylinh')
+    .connect('mongodb://localhost:27017/ngochuyen')
     .then(() => console.log('Connected!'))
     .catch(() => console.log('Not connected!!'));
 };
